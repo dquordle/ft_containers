b3f69d1547231f3cac6bedfc023dc0c9de2d81b5
dquordle <fedka.bogatov@gmail.com>
2021-08-20 02:09:06 +0300

    stack ready

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 	namespace ft = std;
 #else
 //	#include "map.hpp"
-//	#include "stack.hpp"
+	#include "stack.hpp"
 	#include "vector.hpp"
 #endif
 
@@ -25,24 +25,24 @@ struct Buffer
 
 #define COUNT (MAX_RAM / (int)sizeof(Buffer))
 
-//template<typename T>
-//class MutantStack : public ft::stack<T>
-//{
-//public:
-//	MutantStack() {}
-//	MutantStack(const MutantStack<T>& src) { *this = src; }
-//	MutantStack<T>& operator=(const MutantStack<T>& rhs)
-//	{
-//		this->c = rhs.c;
-//		return *this;
-//	}
-//	~MutantStack() {}
-//
-//	typedef typename ft::stack<T>::container_type::iterator iterator;
-//
-//	iterator begin() { return this->c.begin(); }
-//	iterator end() { return this->c.end(); }
-//};
+template<typename T>
+class MutantStack : public ft::stack<T>
+{
+public:
+	MutantStack() {}
+	MutantStack(const MutantStack<T>& src) { *this = src; }
+	MutantStack<T>& operator=(const MutantStack<T>& rhs)
+	{
+		this->c = rhs.c;
+		return *this;
+	}
+	~MutantStack() {}
+
+	typedef typename ft::stack<T>::container_type::iterator iterator;
+
+	iterator begin() { return this->c.begin(); }
+	iterator end() { return this->c.end(); }
+};
 
 int main(int argc, char** argv) {
 	if (argc != 2)
@@ -71,9 +71,9 @@ int main(int argc, char** argv) {
 //	}
 
 	ft::vector<int> vector_int;
-//	ft::stack<int> stack_int;
+	ft::stack<int> stack_int;
 	ft::vector<Buffer> vector_buffer;
-////	ft::stack<Buffer, std::deque<int> > stack_deq_buffer;
+	ft::stack<Buffer, std::deque<int> > stack_deq_buffer;
 //	ft::map<int, int> map_int;
 //
 	for (int i = 0; i < COUNT; i++)
@@ -118,15 +118,15 @@ int main(int argc, char** argv) {
 //	{
 //		ft::map<int, int> copy = map_int;
 //	}
-//	MutantStack<char> iterable_stack;
-//	for (char letter = 'a'; letter <= 'z'; letter++)
-//		iterable_stack.push(letter);
-//	for (MutantStack<char>::iterator it = iterable_stack.begin(); it != iterable_stack.end(); it++)
-//	{
-//		std::cout << *it;
-//	}
-//	std::cout << std::endl;
-//
+	MutantStack<char> iterable_stack;
+	for (char letter = 'a'; letter <= 'z'; letter++)
+		iterable_stack.push(letter);
+	for (MutantStack<char>::iterator it = iterable_stack.begin(); it != iterable_stack.end(); it++)
+	{
+		std::cout << *it;
+	}
+	std::cout << std::endl;
+
 	ft::vector<int> *normal = new ft::vector<int>();
 	std::cout << normal->capacity() << std::endl;
 //
---
+++ stack.hpp
@@ -0,0 +1,105 @@
+#ifndef STACK_HPP
+#define STACK_HPP
+
+#include "vector.hpp"
+
+namespace ft
+{
+	template <typename T, class Container = ft::vector<T> >
+	class stack
+	{
+
+	public:
+		typedef	Container								container_type;
+		typedef size_t									size_type;
+		typedef	T										value_type;
+
+
+
+		explicit stack (const container_type& ctnr = container_type()) : c(ctnr) {}
+
+		bool empty() const
+		{
+			return c.empty();
+		}
+
+		size_type size() const
+		{
+			return c.size();
+		}
+
+		value_type& top()
+		{
+			return c.back();
+		}
+
+		const value_type& top() const
+		{
+			return c.back();
+		}
+
+		void push (const value_type& val)
+		{
+			c.push_back(val);
+		}
+
+		void pop()
+		{
+			c.pop_back();
+		}
+
+	private:
+		template <class Y, class X>
+		friend bool operator == (const stack<Y, X> &lhs, const stack<Y, X> &rhs);
+		template <class Y, class X>
+		friend bool operator != (const stack<Y, X> &lhs, const stack<Y, X> &rhs);
+		template <class Y, class X>
+		friend bool operator > (const stack<Y, X> &lhs, const stack<Y, X> &rhs);
+		template <class Y, class X>
+		friend bool operator >= (const stack<Y, X> &lhs, const stack<Y, X> &rhs);
+		template <class Y, class X>
+		friend bool operator < (const stack<Y, X> &lhs, const stack<Y, X> &rhs);
+		template <class Y, class X>
+		friend bool operator <= (const stack<Y, X> &lhs, const stack<Y, X> &rhs);
+
+	protected:
+		container_type c;
+	};
+
+	template <class T, class Container>
+	bool operator== (const stack<T,Container>& lhs, const stack<T,Container>& rhs)
+	{
+		return lhs.c == rhs.c;
+	}
+
+	template <class T, class Container>
+	bool operator!= (const stack<T,Container>& lhs, const stack<T,Container>& rhs)
+	{
+		return lhs.c != rhs.c;
+	}
+
+	template <class T, class Container>
+	bool operator<  (const stack<T,Container>& lhs, const stack<T,Container>& rhs)
+	{
+		return lhs.c < rhs.c;
+	}
+
+	template <class T, class Container>
+	bool operator<= (const stack<T,Container>& lhs, const stack<T,Container>& rhs)
+	{
+		return lhs.c <= rhs.c;
+	}
+
+	template <class T, class Container>
+	bool operator>  (const stack<T,Container>& lhs, const stack<T,Container>& rhs)
+	{
+		return lhs.c > rhs.c;
+	}
+
+	template <class T, class Container>
+	bool operator>= (const stack<T,Container>& lhs, const stack<T,Container>& rhs)
+	{
+		return lhs.c >= rhs.c;
+	}
+}
+#endif
